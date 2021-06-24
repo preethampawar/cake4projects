c386ff92577c91a1938de1196805d2238824d58c
Preetham Kumar Pawar <preetham.pawar@realpage.com>
2021-06-24 17:17:39 +0530

    ui changes

--- sericulture/templates/Transactions/select.php
+++ sericulture/templates/Transactions/select.php
@@ -1,16 +1,19 @@
 
 
-<div class="text-end">
-    <a class="btn btn-sm btn-danger" href="/Transactions/">Cancel</a>
-</div>
+
 
 <div class="text-center mt-4">
     <h5 class="text-muted">Select Transaction Type</h5>
 
-    <div class="mt-4">
-        <a href="/Transactions/add/expense" class="btn btn-orange w-75">Expense</a>
+    <div class="mt-5">
+        <a href="/Transactions/add/expense" class="btn btn-orange btn-lg w-75">Expense</a>
     </div>
     <div class="mt-4">
-        <a href="/Transactions/add/income" class="btn btn-primary w-75">Income</a>
+        <a href="/Transactions/add/income" class="btn btn-primary btn-lg w-75">Income</a>
     </div>
+</div>
+
+
+<div class="text-center mt-5">
+    <a class="btn btn-sm btn-danger" href="/Transactions/">Cancel</a>
 </div>
